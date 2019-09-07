24a4161962f5b74495af75a3cc33b65dd96f3b67
Khalil Himura <khalil.himura@gmail.com>
2019-09-08 01:32:28 +0800

    Update User.js
    Create a method to get the JSON representation of a user for authentication.

--- models/User.js
+++ models/User.js
@@ -53,4 +53,14 @@ UserSchema.methods.generateJWT = function() {
   }, secret);
 };
 
+UserSchema.methods.toAuthJSON = function() {
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJWT(),
+    bio: this.bio,
+    image: this.image
+  };
+};
+
 mongoose.model('User', UserSchema);
