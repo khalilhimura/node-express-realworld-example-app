27af72a3c8d39f911b7b6662f5ac2c54239acfa8
Khalil Himura <khalil.himura@gmail.com>
2019-09-07 23:42:47 +0800

    Update User.js
    Add validations to the User model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,20 @@
 var mangoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
+  username: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'], // alphanumerical regex
+    index: true
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: [true, "can't be blank"],
+    match: [/\S+@\S+\.\S+/, 'is invalid'], // email regex
+    index: true
+  },
   bio: String,
   image: String,
   hash: String,
