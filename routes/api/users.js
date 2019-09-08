3214b1d4adc68b15aef427bb446b765f0be945a5
Khalil Himura <khalil.himura@gmail.com>
2019-09-08 19:33:27 +0800

    Update users.js
    Create the login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,4 +16,25 @@ router.post('/users', function(req, res, next){
   }).catch(next);
 });
 
+router.post('/users/login', function(req, res, next){
+  if(!req.body.user.email){
+    return res.status(422).json({errors: {email: "can't be blank"}});
+  }
+
+  if(!req.body.user.password){
+    return res.status(422).json({errors: {password: "can't be blank"}});
+  }
+
+  passport.authenticate('local', {session: false}, function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      user.token = user.generateJWT();
+      return res.json({user: user.toAuthJSON()});
+    } else {
+      return res.status(422).json(info);
+    }
+  })(req, res, next);
+});
+
 module.exports = router;
