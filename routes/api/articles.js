8b23011e3f2577c31d26ca7244d3741a8f77440e
Khalil Himura <khalil.himura@gmail.com>
2019-09-09 22:05:04 +0800

    Update articles.js
    Create an endpoint for favoriting an article in routes/api/articles.js

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -79,4 +79,19 @@ router.delete('/:article', auth.required, function(req, res, next){
   });
 });
 
+// Favorite an article
+router.post('/:article/favorite', auth.required, function(req, res, next){
+  var articleId = req.article._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    return user.favorite(articleId).then(function(){
+      return req.article.updateFavoriteCount().then(function(article){
+        return res.json({article: article.toJSONFor(user)});
+      });
+    });
+  }).catch(next);
+});
+
 module.exports = router;
