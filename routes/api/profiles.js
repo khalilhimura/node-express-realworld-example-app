1ec86c8329c0bb20e20c350ed2a3e2a98fc41a11
Khalil Himura <khalil.himura@gmail.com>
2019-09-09 14:42:57 +0800

    Update profiles.js
    Prepopulate req.profile with the user's data when the :username parameter is contained within a route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -3,4 +3,14 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
+router.param('username', function(req, res, next, username){
+  User.findOne({username: username}).then(function(user){
+    if (!user) { return res.sendStatus(404); }
+
+    req.profile = user;
+
+    return.next();
+  }).catch(next);
+});
+
 module.exports = router;
