6707979a9866914954188c40ad5209d927b830dd
Khalil Himura <khalil.himura@gmail.com>
2019-09-09 16:44:18 +0800

    Update articles.js
    Make the endpoint for creating articles

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -5,4 +5,19 @@ var Article = mongoose.model('Article');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
+router.post('/', auth.required, function(req, res, next){
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    var article = new Article(req.body.article);
+
+    article.author = user;
+
+    return article.save().then(function(){
+      console.log(article.author);
+      return res.json({article: article.toJSONFor(user)});
+    });
+  }).catch(next);
+});
+
 module.exports = router;
