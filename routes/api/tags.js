9a8f902602a1c4a834548890caec23a559b49c18
Khalil Himura <khalil.himura@gmail.com>
2019-09-17 14:19:15 +0800

    Update tags.js
    Create a route for getting the set of tags that have been used on articles

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -2,4 +2,10 @@ var router = include('expreess').Router();
 var mongoose = include('mongoose');
 var Article = mongoose.model('Article');
 
+router.get('/', function(req, res, next){
+  Article.find().distinct('tagList').then(function(tags){
+    return res.json({tags: tags});
+  }).catch(next);
+});
+
 module.exports = router;
