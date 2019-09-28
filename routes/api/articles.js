6abb72d5b22992cb04dc42ed10da4056addb641f
Khalil Himura <khalil.himura@gmail.com>
2019-09-29 07:35:11 +0800

    Update articles.js
    Add the ability to filter articles by author and favoriter

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -60,25 +60,43 @@ router.get('/', auth.optional, function(req, res, next){
     query.tagList = {"$in" : [req.query.tag]};
   }
 
-  return Promise.all([
-    Article.find(query)
-      .limit(Number(limit))
-      .skip(Number(offset))
-      .sort({createdAt: 'desc'})
-      .populate('author')
-      .exec(),
-    Article.count(query).exec(),
-    req.payload ? User.findById(req.payload.id) : null,
+  Promise.all([
+    req.query.author ? User.findOne({username: req.query.author}) : null,
+    req.query.favorited ? User.findOne({username: req.query.favorited}) : null
   ]).then(function(results){
-    var articles = results[0];
-    var articlesCount = results[1];
-    var user = results[2];
+    var author = results[0];
+    var favoriter = results[1];
 
-    return res.json({
-      articles: article.map(function(article){
-        return article.toJSONFor(user);
-      }),
-      articlesCount: articlesCount
+    if(author){
+      query.author = author._id;
+    }
+
+    if(favoriter){
+      query._id = {$in: favoriter.favorites};
+    } else if(req.query.favorited){
+      query._id = {$in: []};
+    }
+
+    return Promise.all([
+      Article.find(query)
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .sort({createdAt: 'desc'})
+        .populate('author')
+        .exec(),
+      Article.count(query).exec(),
+      req.payload ? User.findById(req.payload.id) : null,
+    ]).then(function(results){
+      var articles = results[0];
+      var articlesCount = results[1];
+      var user = results[2];
+
+      return res.json({
+        articles: article.map(function(article){
+          return article.toJSONFor(user);
+        }),
+        articlesCount: articlesCount
+      });
     });
   }).catch(next);
 });
